bc32d2aea73bde415e1922aed38dd6b0f083ddce
Jaap Roes <jaap@eight.nl>
2012-07-02 13:32:45 +0200

    HTML cleanup

--- src/js/all.js
+++ src/js/all.js
@@ -1,5 +1,17 @@
 var wall = new Wall("#tweets", settings.getCurrentSettings());
 
+$('.settings').click(function (e) {
+	e.preventDefault(); settings.toggleSettings();
+});
+
+$('.fullscreen').click(function (e) {
+	e.preventDefault(); utils.requestFullscreen(document.getElementById('tweets'));
+});
+
+$('#settingsForm').submit(function (e) {
+	e.preventDefault(); applySettings();
+});
+
 function applySettings () {
 	wall.clear();
 	wall.settings(settings.getCurrentSettings());
